25fb3b8d3936560043647030a17f6fc757fd3e17
Italo Tobias <tobiasitalo@gmail.com>
2023-04-15 22:28:12 -0300

    refactor: conditional rendering for when there are no registered expenses

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -24,63 +24,66 @@ class Table extends Component {
     } = this.props;
 
     return (
-      <table border={ 1 }>
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            expenses.map(({
-              id,
-              description,
-              tag,
-              method,
-              value,
-              currency,
-              exchangeRates,
-            }) => (
-              <tr key={ id }>
-                <td>{ description }</td>
-                <td>{ tag }</td>
-                <td>{ method }</td>
-                <td>{ (Number(value)).toFixed(2) }</td>
-                <td>{ exchangeRates[currency].name }</td>
-                <td>{ (Number(exchangeRates[currency].ask)).toFixed(2) }</td>
-                <td>
-                  { (Number(value) * Number(exchangeRates[currency].ask)).toFixed(2) }
-                </td>
-                <td>Real</td>
-                <td>
-                  <button
-                    type="button"
-                    data-testid="edit-btn"
-                    onClick={ () => this.handleClickUpdate(id) }
-                  >
-                    Editar
-                  </button>
-                  <button
-                    type="button"
-                    data-testid="delete-btn"
-                    onClick={ () => this.handleClickDelete(id) }
-                  >
-                    Excluir
-                  </button>
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+      <>
+        <table border={ 1 }>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              expenses?.map(({
+                id,
+                description,
+                tag,
+                method,
+                value,
+                currency,
+                exchangeRates,
+              }) => (
+                <tr key={ id }>
+                  <td>{ description }</td>
+                  <td>{ tag }</td>
+                  <td>{ method }</td>
+                  <td>{ (Number(value)).toFixed(2) }</td>
+                  <td>{ exchangeRates[currency].name }</td>
+                  <td>{ (Number(exchangeRates[currency].ask)).toFixed(2) }</td>
+                  <td>
+                    { (Number(value) * Number(exchangeRates[currency].ask)).toFixed(2) }
+                  </td>
+                  <td>Real</td>
+                  <td>
+                    <button
+                      type="button"
+                      data-testid="edit-btn"
+                      onClick={ () => this.handleClickUpdate(id) }
+                    >
+                      Editar
+                    </button>
+                    <button
+                      type="button"
+                      data-testid="delete-btn"
+                      onClick={ () => this.handleClickDelete(id) }
+                    >
+                      Excluir
+                    </button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+        { expenses.length === 0 && <div>Nenhum despesa registrada até o momento...</div> }
+      </>
     );
   }
 }
